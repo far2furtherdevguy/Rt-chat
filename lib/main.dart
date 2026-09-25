7a5e5de28712b24ac6bdb8a94abeb27e021cecac
far2furtherdevguy <far2furtherdev@gmail.com>
2026-09-25 15:08:26 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,9 @@ import 'dart:io';
 import 'dart:isolate' show ReceivePort;
 import 'dart:math' as math;
 import 'dart:ui' show IsolateNameServer, PlatformDispatcher;
+import 'package:flutter/foundation.dart' show kDebugMode;
 
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/foundation.dart' show kDebugMode;
 import 'package:cryptography/cryptography.dart' as cr;
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart' as fb;
